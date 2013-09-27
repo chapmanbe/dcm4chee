df545bf021bc965d55782b6b003ad34f3560fbdc
Björn Albers <bjoernalbers@googlemail.com>
2013-09-27 19:23:54 +0200

    Bump bugfix version.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "bjoernalbers@googlemail.com"
 license          "Apache 2.0"
 description      "Installs/Configures dcm4chee"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 depends          "mysql"
 depends          "database"
 depends          "java"
