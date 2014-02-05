64b3130acd56cd2097c436dca7a6fafbd2728f5e
Björn Albers <bjoernalbers@googlemail.com>
2014-02-05 13:30:29 +0100

    Hardcoded basedir for weasis and version bump.

--- recipes/weasis.rb
+++ recipes/weasis.rb
@@ -3,8 +3,10 @@
 # TODO: copy the config files as templates!
 # TODO: Restart dcm4chee afterwarts if any file was downloaded, new created or changed!
 
+basedir = '/usr/local/dcm4chee' # TODO: Refactor this!
+
 node[:dcm4chee][:weasis][:remote_files].each do |file|
-  destination = File.join basedir(:dcm4chee), 'server', 'default', 'deploy',
+  destination = File.join basedir, 'server', 'default', 'deploy',
     file[:source].split('/').last
   remote_file destination do
     source   file[:source]
